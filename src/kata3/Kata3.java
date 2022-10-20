c6a974cfb1a40a76b7a0d1d26cffd42c0d966cf7
Entrar <Entrar@10.22.146.75>
2022-10-20 10:37:29 +0100

    Kata 3 Version 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -6,7 +6,24 @@ public class Kata3 {
 
 
     public static void main(String[] args) {
-        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM DISPLAY...");
+        Histogram<String> histogram = new Histogram();
+        
+        histogram.increment("Ulpgc.es");
+        histogram.increment("Ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("outlook.com");
+        histogram.increment("gmail.com");
+        histogram.increment("Ulpgc.es");
+        histogram.increment("outlook.com");
+        histogram.increment("Ulpgc.es");
+        histogram.increment("Ulpgc.es");
+       
+        
+        
+        
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram, "HISTOGRAM DISPLAY...");
         histogramDisplay.execute();
         
     }
